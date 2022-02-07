e5a625cf3d3b6b6ea59d918c4902cec983a99b3f
CristianCandellone <98396250+CristianCandellone@users.noreply.github.com>
2022-02-07 12:32:23 +0100

    aggiunta form nome e cognome

---
+++ WebAppCandellone/WebAppCandellone/Pages/Risposta.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppCandellone.Pages
+{
+    public class RispostaModel : PageModel
+    {
+        public string TestoUtente { get; set; }
+        public void OnGet(string testo)
+        {
+            TestoUtente = testo;
+        }
+    }
+}
